cd0e8e3ba4538103f0cca20c62d9363d21f40a87
HeyImBrian <78251774+HeyImBrian@users.noreply.github.com>
2022-01-24 20:59:20 -0800

    Added more images, imported images

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './simplebar.min.css';
 import './App.css';
 
 function App() {
+
+  function importAllPictures(pictures) {
+    return pictures.keys().map(pictures);
+  }
   
+  const imagesAppointmentProgram = importAllPictures(require.context("./components/images/projects/appointment-customer-program", false, /\.(png|jp?g)$/));
+  const imagesInventoryProgram = importAllPictures(require.context("./components/images/projects/inventory-program", false, /\.(png|jp?g)$/));
+  const mooloolabaSite = importAllPictures(require.context("./components/images/projects/mooloolaba-site", false, /\.(png|jp?g)$/));
+
   return (
     <div className="backgroundImg">
       <img src={logo} className="myName" alt="logo" />
@@ -19,5 +27,8 @@ function App() {
     </div>
   );
 }
+
+
+
 
 export default App;
